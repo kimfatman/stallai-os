3bf31314f568c0310beaea4347a39f0147f5f8d7
kimfatman <237029918@QQ.com>
2026-05-14 09:31:40 +0800

    Add stallai-backend/src/common/guards/jwt-auth.guard.ts

---
+++ stallai-backend/src/common/guards/jwt-auth.guard.ts
@@ -0,0 +1,10 @@
+/**
+ * JWT 认证守卫
+ * JWT Auth Guard
+ */
+
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {}
