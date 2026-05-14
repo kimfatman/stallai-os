1c77f30ded98bdf8c067c0f7740858c0fd87c21c
kimfatman <237029918@QQ.com>
2026-05-14 09:32:21 +0800

    Add stallai-backend/src/modules/products/products.module.ts

---
+++ stallai-backend/src/modules/products/products.module.ts
@@ -0,0 +1,15 @@
+/**
+ * 商品模块
+ * Products Module
+ */
+
+import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+
+@Module({
+  controllers: [ProductsController],
+  providers: [ProductsService],
+  exports: [ProductsService],
+})
+export class ProductsModule {}
