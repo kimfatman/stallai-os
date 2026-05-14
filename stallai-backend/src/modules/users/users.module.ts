dbdc34a533775d763c98b0fbc359037ab4cbaf11
kimfatman <237029918@QQ.com>
2026-05-14 09:32:30 +0800

    Add stallai-backend/src/modules/users/users.module.ts

---
+++ stallai-backend/src/modules/users/users.module.ts
@@ -0,0 +1,15 @@
+/**
+ * 用户模块
+ * Users Module
+ */
+
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
